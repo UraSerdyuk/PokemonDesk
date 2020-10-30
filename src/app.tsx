66c2b7e30d93fcaaf0cdd90f0ba05f23b98c1f75
UraSerdyuk <yuraserdiuk@gmail.com>
2020-10-30 17:32:10 +0100

    homework-2

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import cn from "classnames";
 
 import "./custom.css";
-import s from "./App.modules.scss";
+// import s  from "./App.modules.scss";
 
 const App = () => {
-  return <div className={cn(s.header, "color")}>This is App component !</div>;
+  return <div >This is App component !</div>;
 };
 
 export default App;
