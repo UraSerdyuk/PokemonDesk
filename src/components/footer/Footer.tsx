04e7f1f4327010c04da7b266d4d9901e55e8d749
UraSerdyuk <yuraserdiuk@gmail.com>
2020-11-02 22:41:20 +0100

    test

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ import s from './Footer.module.scss';
 const Footer = () => {
   return (
     <div className={cn(s.footer)}>
-      <span>footer</span> <span>Ours Team</span>
+      <span>Make with</span> <span>Ours Team</span>
     </div>
   );
 };
